2e8e9c3fe91707c97dae046f331b88ad6b869f44
Warwick Foster <warwick@foster.net>
2021-03-28 12:25:49 +1100

    latest

--- UnitTestProject1/WeatherForecastController.cs
+++ UnitTestProject1/WeatherForecastController.cs
@@ -1,28 +1,28 @@
-using demoApp;
-using System.Linq;
-using demoApp.Controllers;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
-using Microsoft.Extensions.Logging;
+//using demoApp;
+//using System.Linq;
+//using demoApp.Controllers;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using System.Collections.Generic;
+//using Microsoft.Extensions.Logging;
 
-namespace demoTest
-{
-    [TestClass]
-    public class TestWeatherForecastController
-    {
-        WeatherForecastController weatherForecastController;
-        IEnumerable<WeatherForecast> weatherForecastResponse;
+//namespace demoTest
+//{
+//    [TestClass]
+//    public class TestWeatherForecastController
+//    {
+//        WeatherForecastController weatherForecastController;
+//        IEnumerable<WeatherForecast> weatherForecastResponse;
 
-        [TestInitialize]
-        public void Setup()
-        {
-            weatherForecastController = new WeatherForecastController(null);
-            weatherForecastResponse = weatherForecastController.Get();
-        }
+//        [TestInitialize]
+//        public void Setup()
+//        {
+//            weatherForecastController = new WeatherForecastController(null);
+//            weatherForecastResponse = weatherForecastController.Get();
+//        }
 
-        [TestMethod] public void WeatherForecastControllerIsNotNull() => Assert.IsNotNull(weatherForecastController);
-        [TestMethod] public void GetWeatherForecastIsNotNull() => Assert.IsNotNull(weatherForecastResponse);
-        [TestMethod] public void GetWeatherForecastHasContent() => Assert.AreNotEqual(0, weatherForecastResponse.ToList().Count);
-        [TestMethod] public void GetWeatherForecastHas5Rows() => Assert.AreEqual(5, weatherForecastResponse.ToList().Count);
-    }
-}
+//        [TestMethod] public void WeatherForecastControllerIsNotNull() => Assert.IsNotNull(weatherForecastController);
+//        [TestMethod] public void GetWeatherForecastIsNotNull() => Assert.IsNotNull(weatherForecastResponse);
+//        [TestMethod] public void GetWeatherForecastHasContent() => Assert.AreNotEqual(0, weatherForecastResponse.ToList().Count);
+//        [TestMethod] public void GetWeatherForecastHas5Rows() => Assert.AreEqual(5, weatherForecastResponse.ToList().Count);
+//    }
+//}
